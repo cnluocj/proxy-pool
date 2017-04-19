f719254c8d7fb1db767d33e220eb7b5acd2b57a2
cnluocj <cnluocj@gmail.com>
2017-04-19 21:58:43 +0800

    no message

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 # coding:utf-8
 
 
-import json
-from spider.ProxySpiderFactory import ProxySpiderFactory
 from spider.ProxyValidator import ProxyValidator
-from spider.ProxyGetter import ProxyGetter
-from db.RedisClient import RedisClient
 from api import ProxyApi
 
 
@@ -16,11 +12,6 @@ def run():
 
     # 启动 Flask 服务器
     ProxyApi.app.run()
-
-    # getter = ProxyGetter()
-    # for x in range(0, 100):
-    #     print x
-    #     print json.dumps(getter.get())
 
 
 if __name__ == '__main__':
