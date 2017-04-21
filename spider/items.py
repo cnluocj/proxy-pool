9a1630427df4f9f537d3214d1dab377b23c2c3c7
cnluocj <cnluocj@gmail.com>
2017-04-21 17:05:33 +0800

    使用 property 生成器替换以前的做法

--- spider/items.py
+++ spider/items.py
@@ -4,37 +4,40 @@ import json
 
 
 class Proxy(object):
-    def __init__(self, ip='', port='', faceless='', type=''):
+    def __init__(self, ip='', port='', faceless='', ty=''):
         self.ip = ip
         self.port = port
         self.faceless = faceless
-        self.type = type
+        self.type = ty
+        self.valid_at = ''
 
-    def __setattr__(self, key, value):
-        if 'faceless' == key:
-            self.set_faceless(value);
-        elif 'type' == key:
-            self.set_type(value)
-        else:
-            self.__dict__[key] = value
+    @property
+    def faceless(self):
+        return self._faceless
 
-    def set_faceless(self, value):
+    @faceless.setter
+    def faceless(self, value):
         if u'透明' in value:
-            self.__dict__['faceless'] = u'透明'
+            self._faceless = u'透明'
         elif u'高匿' in value:
-            self.__dict__['faceless'] = u'高匿'
+            self._faceless = u'高匿'
         elif u'匿名' in value:
-            self.__dict__['faceless'] = u'匿名'
+            self._faceless = u'匿名'
         else:
-            self.__dict__['faceless'] = value
+            self._faceless = value
 
-    def set_type(self, value):
+    @property
+    def type(self):
+        return self._type
+
+    @type.setter
+    def type(self, value):
         if 'HTTPS' in value or 'https' in value:
-            self.__dict__['type'] = 'HTTPS'
+            self._type = 'HTTPS'
         elif 'HTTP' in value or 'http' in value:
-            self.__dict__['type'] = 'HTTP'
+            self._type = 'HTTP'
         else:
-            self.__dict__['type'] = value
+            self._type = value
 
     def to_dict(self):
         return {
@@ -58,3 +61,9 @@ class Proxy(object):
         proxy.faceless = object_dict['faceless']
         proxy.type = object_dict['type']
         return proxy
+
+if __name__ == '__main__':
+    proxy = Proxy(faceless=u'透明', ty='https')
+    # proxy.faceless = u'透明'
+    # proxy.type = 'http'
+    print proxy.to_string()
